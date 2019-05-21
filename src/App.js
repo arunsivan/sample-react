f969e22afca9efa4a848332cc10de5c765003573
arunsivan18.8@gmail.com <arunsivan18.8@gmail.com>
2019-05-21 23:03:23 +0530

    sample ract first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Welcome name="pop"></Welcome>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
